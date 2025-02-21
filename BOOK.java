70fea4687ed153b8b84bfa9f110d787c8ee1e630
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-02-21 11:58:40 +0500

    Add files via upload

---
+++ BOOK.java
@@ -0,0 +1,32 @@
+BOOK
+	name
+	author
+	edition
+	ISBN
+	category
+	avail_status
+
+display_details()
+
+
+Member
+	ID
+	name
+	cont_info
+Register()
+check_avail(Book b)
+
+Readers
+	num_of_borrowed books
+borrow_books(Book b)
+
+
+Librarian/Library Staff
+due date
+borrow date
+
+issued Book(Book b)
+report(due_date, borrow_date,reader m)
+add_book( Book b)
+update_book_avail( Book b)
+remove_book(Book b)// optional
