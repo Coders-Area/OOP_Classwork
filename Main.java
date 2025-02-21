2cce0cf9b3bb885c45d22ec4dcf098e9576dced8
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-02-21 11:48:24 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Student obj1 = new Student();
+        obj1.name="Ali";
+        obj1.rollno=1;
+        obj1.cgpa=3;
+        obj1.display();
+
+
+        }
+    }
