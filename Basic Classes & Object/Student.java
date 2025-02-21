5e8319da85f9593b4400c71df9d6a0959adc1885
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-02-21 11:52:59 +0500

    Add files via upload

---
+++ Basic Classes & Object/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    String name;
+    int cgpa;
+     int rollno;
+
+
+     void display(){
+         System.out.println("The name of a Student is "+name);
+         System.out.println("The cgpa of a Student is "+cgpa);
+     }
+
+}
