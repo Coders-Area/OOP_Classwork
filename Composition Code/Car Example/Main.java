36534c0f71bfe33c707bcb040d2cf2650b2d20c0
Muhammad Usman Pirzada <k243000@nu.edu.pk>
2025-02-22 18:01:11 +0500

    Create Main.java

---
+++ Composition Code/Car Example/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Engine e1 = new Engine(1000,12345,45678,"Japnese");
+       Car c1 = new Car("Mehran","Asd5652","2012","Suzuki",e1);
+       c1.Start_Car();
+       c1.Stop_Car();
+       c1.display_car_details();
+    }
