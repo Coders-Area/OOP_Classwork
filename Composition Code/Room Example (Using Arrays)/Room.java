80d5f1fcb990c1b5ded420bb78f61c7bb1a379c2
Muhammad Usman Pirzada <k243000@nu.edu.pk>
2025-02-22 18:04:12 +0500

    Create Room.java

---
+++ Composition Code/Room Example (Using Arrays)/Room.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+// Room class representing a room in a house.
+class Room {
+    private int roomNumber;
+    private String roomType;
+
+    public Room(int roomNumber, String roomType) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void displayRoomInfo() {
+        System.out.println("Room Number: " + roomNumber + ", Room Type: " + roomType);
+    }
+}
