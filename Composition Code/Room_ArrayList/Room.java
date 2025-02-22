7b03cf477b23d61c0c5be54cebe86786c22c840a
Muhammad Usman Pirzada <k243000@nu.edu.pk>
2025-02-22 18:08:10 +0500

    Create Room.java

---
+++ Composition Code/Room_ArrayList/Room.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Room class representing a room in a house.
+class Room {
+    private int roomNumber;
+    private String roomType;
+
+    public Room(int roomNumber, String roomType) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void displayRoomInfo() {
+        System.out.println("Room Number: " + roomNumber + ", Room Type: " + roomType);
+    }
+}
